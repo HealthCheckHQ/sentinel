b8b993d7b8d71ef24534e4ad18a2a0d4331fd69b
Avneesh Kumar <avneesh.kumar051292@gmail.com>
2024-09-13 18:53:17 +0530

    doc: add readme and other documentation

--- src/models/users.model.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '@interfaces/users.interface';
-
-// password: password123456789
-export const UserModel: User[] = [
-  { id: 1, email: 'example1@email.com', password: '$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u' },
-  { id: 2, email: 'example2@email.com', password: '$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u' },
-  { id: 3, email: 'example3@email.com', password: '$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u' },
-  { id: 4, email: 'example4@email.com', password: '$2b$10$2YC2ht8x06Yto5VAr08kben8.oxjTPrMn0yJhv8xxSVVltH3bOs4u' },
-];
